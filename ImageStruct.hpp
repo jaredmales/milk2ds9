6b536a0e5c1f9590fb6efc73bc78940885ae4c75
Jared Males <jaredmales@gmail.com>
2018-09-20 14:26:35 -0700

    handles size changes, only needs image name not full path

--- ImageStruct.hpp
+++ ImageStruct.hpp
@@ -189,4 +189,126 @@ struct imageStructDataType<_DATATYPE_EVENT_UI8_UI8_UI16_UI8>
    static void setPointer( IMAGE & im, char * mapv) { im.array.event1121 = (type *) mapv;}
 };
 
+
+inline
+int openShMem( IMAGE & image,          ///< [out] A real-time image structure which contains the image data and meta-data.
+               size_t & type_size,     ///< [out] The size of the data type in the images.
+               int & bitpix,             ///< [out] The FITS standard bitpix value for the type in the images.
+               sem_t* & sem,           ///< [out] The semaphore to be initialized.
+               int semaphoreNumber,  ///< [in] The semaphore number to watch.
+               const std::string & shmem_key ///< [in] The filename of the shared memory buffer.
+             )
+{
+   int SM_fd;
+   struct stat file_stat;
+   IMAGE_METADATA *map;
+
+   SM_fd = open(shmem_key.c_str(), O_RDWR);
+
+   char *mapv;
+
+   if(SM_fd==-1)
+   {
+      return -1;
+   }
+   else
+   {
+      fstat(SM_fd, &file_stat);
+
+      map = (IMAGE_METADATA*) mmap(0, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, SM_fd, 0);
+
+      if (map == MAP_FAILED)
+      {
+        ::close(SM_fd);
+        perror("Error mmapping the file");
+        return -1;
+      }
+
+      std::cerr << "Attached to " << shmem_key << "\n";
+
+      image.memsize = file_stat.st_size;
+      image.shmfd = SM_fd;
+      image.md = map;
+      image.md->shared = 1;
+
+      mapv = (char*) map;
+      mapv += sizeof(IMAGE_METADATA);
+
+      //Make the semaphore file name and try to open it.
+      char sname[200];
+      sprintf(sname, "%s_sem%02ld", image.md[0].name, (long int) semaphoreNumber);
+      if ((sem = sem_open(sname, 0, 0644, 0))== SEM_FAILED)
+      {
+         std::cerr << "Semaphore not open\n";
+         return -1;
+      }
+
+      switch(image.md[0].atype)
+      {
+         case _DATATYPE_UINT8:
+            imageStructDataType<_DATATYPE_UINT8>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_UINT8>::size;
+            bitpix = imageStructDataType<_DATATYPE_UINT8>::bitpix;
+            break;
+         case _DATATYPE_INT8:
+            imageStructDataType<_DATATYPE_INT8>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_INT8>::size;
+            bitpix = imageStructDataType<_DATATYPE_INT8>::bitpix;
+            break;
+         case _DATATYPE_UINT16:
+            imageStructDataType<_DATATYPE_UINT16>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_UINT16>::size;
+            bitpix = imageStructDataType<_DATATYPE_UINT16>::bitpix;
+            break;
+         case _DATATYPE_INT16:
+            imageStructDataType<_DATATYPE_INT16>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_INT16>::size;
+            bitpix = imageStructDataType<_DATATYPE_INT16>::bitpix;
+            break;
+         case _DATATYPE_UINT32:
+            imageStructDataType<_DATATYPE_UINT32>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_UINT32>::size;
+            bitpix = imageStructDataType<_DATATYPE_UINT32>::bitpix;
+            break;
+         case _DATATYPE_INT32:
+            imageStructDataType<_DATATYPE_INT32>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_INT32>::size;
+            bitpix = imageStructDataType<_DATATYPE_INT32>::bitpix;
+            break;
+         case _DATATYPE_UINT64:
+            imageStructDataType<_DATATYPE_UINT64>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_UINT64>::size;
+            bitpix = imageStructDataType<_DATATYPE_UINT64>::bitpix;
+            break;
+         case _DATATYPE_INT64:
+            imageStructDataType<_DATATYPE_INT64>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_INT64>::size;
+            bitpix = imageStructDataType<_DATATYPE_INT64>::bitpix;
+            break;
+         case _DATATYPE_FLOAT:
+            imageStructDataType<_DATATYPE_FLOAT>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_FLOAT>::size;
+            bitpix = imageStructDataType<_DATATYPE_FLOAT>::bitpix;
+            break;
+         case _DATATYPE_DOUBLE:
+            imageStructDataType<_DATATYPE_DOUBLE>::setPointer( image, mapv);
+            type_size = imageStructDataType<_DATATYPE_DOUBLE>::size;
+            bitpix = imageStructDataType<_DATATYPE_DOUBLE>::bitpix;
+            break;
+         default:
+            std::cerr << "Unknown or unsupported data type\n";
+            return -1;
+      }
+
+   }
+
+   return 0;
+}
+
+inline
+int closeShMem( IMAGE & image /**< [in] A real-time image structure which contains the image data and meta-data.*/)
+{
+   return munmap( image.md, image.memsize);
+}
+
 #endif //ImageStruct_hpp
--- milk2ds9.cpp
+++ milk2ds9.cpp
@@ -12,119 +12,7 @@
 
 #include "ImageStruct.hpp"
 
-int openShmem( IMAGE & image,          ///< [out] A real-time image structure which contains the image data and meta-data.
-               size_t & type_size,     ///< [out] The size of the data type in the images.
-               int & bitpix,             ///< [out] The FITS standard bitpix value for the type in the images.
-               sem_t* & sem,           ///< [out] The semaphore to be initialized.
-               int semaphoreNumber,  ///< [in] The semaphore number to watch.
-               const std::string & shmem_key ///< [in] The filename of the shared memory buffer.
-             )
-{
-   int SM_fd;
-   struct stat file_stat;
-   IMAGE_METADATA *map;
 
-   SM_fd = open(shmem_key.c_str(), O_RDWR);
-
-   char *mapv;
-
-   if(SM_fd==-1)
-   {
-      return -1;
-   }
-   else
-   {
-      fstat(SM_fd, &file_stat);
-
-      map = (IMAGE_METADATA*) mmap(0, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, SM_fd, 0);
-
-      if (map == MAP_FAILED)
-      {
-        ::close(SM_fd);
-        perror("Error mmapping the file");
-        return -1;
-      }
-
-      std::cerr << "Attached to " << shmem_key << "\n";
-
-      image.memsize = file_stat.st_size;
-      image.shmfd = SM_fd;
-      image.md = map;
-      image.md->shared = 1;
-
-      mapv = (char*) map;
-      mapv += sizeof(IMAGE_METADATA);
-
-      //Make the semaphore file name and try to open it.
-      char sname[200];
-      sprintf(sname, "%s_sem%02ld", image.md[0].name, (long int) semaphoreNumber);
-      if ((sem = sem_open(sname, 0, 0644, 0))== SEM_FAILED)
-      {
-         std::cerr << "Semaphore not open\n";
-         return -1;
-      }
-
-      switch(image.md[0].atype)
-      {
-         case _DATATYPE_UINT8:
-            imageStructDataType<_DATATYPE_UINT8>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_UINT8>::size;
-            bitpix = imageStructDataType<_DATATYPE_UINT8>::bitpix;
-            break;
-         case _DATATYPE_INT8:
-            imageStructDataType<_DATATYPE_INT8>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_INT8>::size;
-            bitpix = imageStructDataType<_DATATYPE_INT8>::bitpix;
-            break;
-         case _DATATYPE_UINT16:
-            imageStructDataType<_DATATYPE_UINT16>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_UINT16>::size;
-            bitpix = imageStructDataType<_DATATYPE_UINT16>::bitpix;
-            break;
-         case _DATATYPE_INT16:
-            imageStructDataType<_DATATYPE_INT16>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_INT16>::size;
-            bitpix = imageStructDataType<_DATATYPE_INT16>::bitpix;
-            break;
-         case _DATATYPE_UINT32:
-            imageStructDataType<_DATATYPE_UINT32>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_UINT32>::size;
-            bitpix = imageStructDataType<_DATATYPE_UINT32>::bitpix;
-            break;
-         case _DATATYPE_INT32:
-            imageStructDataType<_DATATYPE_INT32>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_INT32>::size;
-            bitpix = imageStructDataType<_DATATYPE_INT32>::bitpix;
-            break;
-         case _DATATYPE_UINT64:
-            imageStructDataType<_DATATYPE_UINT64>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_UINT64>::size;
-            bitpix = imageStructDataType<_DATATYPE_UINT64>::bitpix;
-            break;
-         case _DATATYPE_INT64:
-            imageStructDataType<_DATATYPE_INT64>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_INT64>::size;
-            bitpix = imageStructDataType<_DATATYPE_INT64>::bitpix;
-            break;
-         case _DATATYPE_FLOAT:
-            imageStructDataType<_DATATYPE_FLOAT>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_FLOAT>::size;
-            bitpix = imageStructDataType<_DATATYPE_FLOAT>::bitpix;
-            break;
-         case _DATATYPE_DOUBLE:
-            imageStructDataType<_DATATYPE_DOUBLE>::setPointer( image, mapv);
-            type_size = imageStructDataType<_DATATYPE_DOUBLE>::size;
-            bitpix = imageStructDataType<_DATATYPE_DOUBLE>::bitpix;
-            break;
-         default:
-            std::cerr << "Unknown or unsupported data type\n";
-            return -1;
-      }
-
-   }
-
-   return 0;
-}
 
 
 void usage( const char * argv0,
@@ -174,8 +62,6 @@ have to provide shmem_key as argument
 int main( int argc,
           char ** argv )
 {
-
-   std::string shmem_key;
 
    std::string ds9Title;
    int semaphoreNumber {0}; ///< Number of the semaphore to monitor for new image data.  This determines the filename.
@@ -247,8 +133,10 @@ int main( int argc,
       return -1;
    }
 
-   shmem_key = argv[optind];
-
+   char SM_fname[200];
+   sprintf(SM_fname, "%s/%s.im.shm", SHAREDMEMDIR, argv[optind]);   
+   
+   if(ds9Title == "") ds9Title = argv[optind];
 
    IMAGE image;
 
@@ -258,41 +146,60 @@ int main( int argc,
 
    int bitpix;
 
-   bool opened = false;
-   while(!opened)
-   {
-      if( openShmem( image, type_size, bitpix, sem, semaphoreNumber, shmem_key) == 0) opened = true;
-      usleep(1000);
-   }
-
-   mx::improc::ds9Interface ds9(shmem_key);
-
-   int curr_image;
-   size_t snx, sny;
-
    while(1)
    {
-      if(sem_trywait(sem) == 0)
+      bool opened = false;
+      while(!opened)
       {
-         if(image.md[0].size[2] > 0)
+         if( openShMem( image, type_size, bitpix, sem, semaphoreNumber, SM_fname) == 0) opened = true;
+         usleep(1000);
+      }
+      
+   
+      mx::improc::ds9Interface ds9(ds9Title);
+
+      int curr_image;
+      size_t snx, sny, snz;
+      size_t last_snx = image.md[0].size[0];
+      size_t last_sny = image.md[0].size[1];
+      size_t last_snz = image.md[0].size[2];
+
+      while(1)
+      {
+         if(sem_trywait(sem) == 0)
          {
-            curr_image = image.md[0].cnt1 - 1;
-            if(curr_image < 0) curr_image = image.md[0].size[2] - 1;
+            if(image.md[0].size[2] > 0)
+            {
+               curr_image = image.md[0].cnt1 - 1;
+               if(curr_image < 0) curr_image = image.md[0].size[2] - 1;
+            }
+            else curr_image = 0;
+
+            snx = image.md[0].size[0];
+            sny = image.md[0].size[1];
+            snz = image.md[0].size[2];
+         
+            if( snx != last_snx || sny != last_sny || snz != last_snz )
+            {
+               std::cerr << "Size change detected!\n";
+               break;
+            }
+         
+            ds9.display( (void *) (image.array.SI8 + curr_image*snx*sny*type_size), bitpix, type_size, snx, sny, 1, frameNo);
+         
+            last_snx = snx;
+            last_sny = sny;
+            last_snz = snz;
+         
+            usleep(waitTime);
          }
-         else curr_image = 0;
-
-         snx = image.md[0].size[0];
-         sny = image.md[0].size[1];
-
-         ds9.display( (void *) (image.array.SI8 + curr_image*snx*sny*type_size),
-                           bitpix, type_size, snx, sny, 1, frameNo);
-         usleep(waitTime);
+         else
+         {
+            usleep(pauseTime);
+         }
       }
-      else
-      {
-         usleep(pauseTime);
-      }
+
+      closeShMem(image);
    }
-
    return 0;
 }
